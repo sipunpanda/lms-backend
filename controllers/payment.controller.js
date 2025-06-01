b3371250d8e992f1920eb86e703b4a4f98f98334
sipunpanda <sipunpanda26@gmail.com>
2025-06-02 01:06:57 +0530

    Admin Dashboard Data Fetched

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -152,27 +152,106 @@ export const cancelSubscription = async (req, res, next) => {
     }
 
 
-export const getAllPayments = async (req, res, next) => {
-    try {
-        const { count } = req.query;
-        console.log("heeeeee");
+    // method 1 
+
+// export const getAllPayments = async (req, res, next) => {
+//     try {
+//         const { count } = req.query;
+//         console.log("heeeeee158");
         
 
-        const subscriptions = await razorpay.subscriptions.all({
-            count: count || 10,
-        });
+//         const subscriptions = await razorpay.subscriptions.all({
+//             count: count || 10,
+//         });
 
-        res.status(200).json({
-            success: true,
-            message: "Payments fetched successfully",
-            subscriptions:subscriptions,
-        })
+//         res.status(200).json({
+//             success: true,
+//             message: "Payments fetched successfully",
+//             subscriptions:subscriptions,
+//         })
 
 
-    }
-    catch (error) {
-        return next(new AppError("Unable to get payments", 500));
+//     }
+//     catch (error) {
+//         return next(new AppError("Unable to get payments", 500));
         
-    }
+//     }
 
-}
+// }
+
+
+
+
+
+
+ 
+
+ //method2
+ export const getAllPayments = async (req, res, next) => {
+     const { count, skip } = req.query;
+
+  // Find all subscriptions from razorpay
+  const allPayments = await razorpay.subscriptions.all({
+    count: count ? count : 10, // If count is sent then use that else default to 10
+    skip: skip ? skip : 0, // // If skip is sent then use that else default to 0
+  });
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const finalMonths = {
+    January: 0,
+    February: 0,
+    March: 0,
+    April: 0,
+    May: 0,
+    June: 0,
+    July: 0,
+    August: 0,
+    September: 0,
+    October: 0,
+    November: 0,
+    December: 0,
+  };
+
+  const monthlyWisePayments = allPayments.items.map((payment) => {
+    // We are using payment.start_at which is in unix time, so we are converting it to Human readable format using Date()
+    const monthsInNumbers = new Date(payment.start_at * 1000);
+
+    return monthNames[monthsInNumbers.getMonth()];
+  });
+
+  monthlyWisePayments.map((month) => {
+    Object.keys(finalMonths).forEach((objMonth) => {
+      if (month === objMonth) {
+        finalMonths[month] += 1;
+      }
+    });
+  });
+
+  const monthlySalesRecord = [];
+
+  Object.keys(finalMonths).forEach((monthName) => {
+    monthlySalesRecord.push(finalMonths[monthName]);
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'All payments',
+    allPayments,
+    finalMonths,
+    monthlySalesRecord,
+  });
+  }
